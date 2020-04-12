13c1334ff87066c82bdf526b53691de85ef5488c
Jeff Terry <terryj@iit.edu>
2020-04-12 12:13:14 -0500

    Initial Commit

---
+++ SwiftPlot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftPlot
+//
+//  Created by Jeff Terry on 4/12/20.
+//  Copyright © 2020 Jeff Terry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftPlot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftPlot
+//
+//  Created by Jeff Terry on 4/12/20.
+//  Copyright © 2020 Jeff Terry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
